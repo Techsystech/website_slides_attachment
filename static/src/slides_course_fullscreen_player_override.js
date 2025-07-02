c8c9ad775904f3a6330ea6792eedb961acb5e6a2
Simeon <simeon@cedarcreekfurniture.net>
2025-07-01 19:30:06 -0500

    add async in an attempt to fix error thrown about cannot read properties of undefined. Cannot read completed

--- static/src/slides_course_fullscreen_player_override.js
+++ static/src/slides_course_fullscreen_player_override.js
@@ -9,7 +9,7 @@ Fullscreen.include({
      * @private
      * @override
      */
-    _renderSlide: async function() {
+    _renderSlide: function() {
         var def = this._super.apply(this, arguments);
         if (this._renderSlideRunning) { return; }
         this._renderSlideRunning = true;
@@ -19,9 +19,9 @@ Fullscreen.include({
             if (slide.category === 'video' && slide.videoSourceType === 'local') {
                 $content.empty().append(renderToElement('website.slides.fullscreen.content.video', {widget: this}));
                 const videoViewer = document.querySelector('#embeddedVideoViewer');
-                videoViewer.addEventListener('ended', (event) => {
-                    this.trigger_up('slide_mark_completed', slide);
-                    this.trigger_up('slide_go_next', slide);
+                videoViewer.addEventListener('ended', async (event) => {
+                    await this.trigger_up('slide_mark_completed', slide);
+                    await this.trigger_up('slide_go_next', slide);
                 });
             }
         }
